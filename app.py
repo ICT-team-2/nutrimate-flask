2765df279e32078abe3427f0f78d7f10caa9dba2
jaehak26 <68010205+jaehak26@users.noreply.github.com>
2024-01-10 18:33:06 +0900

    flask project init

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask
+from flask_restful import Api
+from flask_cors import CORS
+import os
+from asgiref.wsgi import WsgiToAsgi
+import uvicorn
+
+app = Flask(__name__)
+CORS(app)
+
+app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'upload')
+app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
+
+api = Api(app)
+
+asgi_app = WsgiToAsgi(app)
+
+if __name__ == '__main__':
+    uvicorn.run(asgi_app, port=5000, host='0.0.0.0')
---
+++ readme.md
@@ -0,0 +1,2 @@
+### 아나콘다 환경 export, import, clone 정리
+https://jh-bk.tistory.com/35
