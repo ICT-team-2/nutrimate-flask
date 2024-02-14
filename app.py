ef9d578e14463149ef20fbe5f832165ad1da405f
jaehak26 <jack10829@naver.com>
2024-02-14 14:16:21 +0900

    ICT2-64 Cors문제 해결

--- app.py
+++ app.py
@@ -6,9 +6,9 @@ from asgiref.wsgi import WsgiToAsgi
 import uvicorn
 
 from api.ChatBot import ChatBot
-#OCR서비스용
+# OCR서비스용
 from api.ocr import OCR
-#ServiceWorker서비스용
+# ServiceWorker서비스용
 from api.ServiceWorker import ServiceWorker
 
 from api.recipe import RecipeResource
@@ -17,38 +17,40 @@ from api.news import Navernews, Exercise, Nutrients
 
 
 app = Flask(__name__)
-CORS(app)
+CORS(app,
+    resources={r'*': {'origins': 'http://localhost:5555'}},
+    supports_credentials=True)
 
 app.config['UPLOAD_FOLDER'] = os.path.join(os.getcwd(), 'upload')
 app.config['MAX_CONTENT_LENGTH'] = 16 * 1024 * 1024  # 16MB
 
 api = Api(app)
 
+
 @app.route('/')
 def home():
     return "Hello, ICT!"
 
-api.add_resource(RecipeResource,'/recipe_info')
+
+api.add_resource(RecipeResource, '/recipe_info')
 
 asgi_app = WsgiToAsgi(app)
 
-api.add_resource(ChatBot,'/chatbot')
-api.add_resource(Navernews,'/navernews')
-api.add_resource(Exercise,'/exercise-info')
-api.add_resource(Nutrients,'/nutrients-info')
+api.add_resource(ChatBot, '/chatbot')
+api.add_resource(Navernews, '/navernews')
+api.add_resource(Exercise, '/exercise-info')
+api.add_resource(Nutrients, '/nutrients-info')
 '''
 OCR
 POST /ocr
 '''
-api.add_resource(OCR,'/ocr')
-
+api.add_resource(OCR, '/ocr')
 
 '''
 ServiceWorker
 Post /serviceworker
 '''
-api.add_resource(ServiceWorker,'/serviceworker')
+api.add_resource(ServiceWorker, '/serviceworker')
 
 if __name__ == '__main__':
     uvicorn.run(asgi_app, port=2222, host='0.0.0.0')
-
