c994ff7c9397c224a937fec4b44c7a4f97c274a8
OHJOOWOO <juoh2011@naver.com>
2024-02-26 12:45:49 +0900

    ICT2-81알레르기 수정

--- api/allergy.py
+++ api/allergy.py
@@ -40,7 +40,15 @@ class Allergy(Resource):
                 recipes = data['hits']
 
                 # 레시피 정보를 총 레시피 리스트에 추가
-                total_recipes += recipes
+                for recipe in recipes:
+                    recipe_info = recipe['recipe']
+                    total_recipes.append({
+                        '이미지': recipe_info['image'],
+                        '음식 이름': recipe_info['label'],
+                        '칼로리': recipe_info['calories']/recipe_info['yield'], # 1인분당 칼로리 정보
+                        '건강 관련 정보': recipe_info['healthLabels'],
+                        '영양소 정보': recipe_info['totalNutrients']
+                    })
 
         # 레시피 정보 반환
         return jsonify(total_recipes)
