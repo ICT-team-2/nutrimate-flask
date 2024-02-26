cbd67b83a33781e797e048bb9c94b4b96ebd74e4
OHJOOWOO <juoh2011@naver.com>
2024-02-26 18:11:27 +0900

    ICT2-71산책코스수정

--- api/walk_course.py
+++ api/walk_course.py
@@ -4,14 +4,15 @@ import pandas as pd
 import json
 
 class WalkCourse(Resource):
-    def post(self):
+    def get(self):
         # csv 파일 읽기
         df = pd.read_csv('data/KC_CFR_WLK_STRET_INFO_2021.csv')
 
         # 사용자 입력 받기
-        place = request.json.get('place')
-        difficulty = request.json.get('difficulty', '쉬움')  # 초기값으로 '쉬움' 설정
-        distance = request.json.get('distance', 1.0)  # 초기값으로 1.0 설정
+        place = request.args.get('place')
+        difficulty = request.args.get('difficulty', None)  # 초기값을 None으로 설정
+        distance = request.args.get('distance', None)  # 초기값을 None으로 설정
+        distance = float(distance) if distance else None
 
         # 거리 컬럼을 숫자로 변환 (범위의 시작과 끝의 평균 계산)
         df['COURS_LT_CN'] = df['COURS_LT_CN'].apply(
@@ -19,9 +20,11 @@ class WalkCourse(Resource):
                        float(x.split('~')[1].replace('Km미만', '').replace('Km이상', ''))) / 2 if '~' in x else float(x.replace('Km미만', '').replace('Km이상', '')))
 
         # 사용자가 입력한 값에 따라 데이터 필터링
-        filtered_df = df[(df['SIGNGU_NM'].str.contains(place)) &
-                         (df['COURS_LEVEL_NM'] == difficulty) &
-                         (df['COURS_LT_CN'] <= distance)]
+        if difficulty:
+            df = df[df['COURS_LEVEL_NM'] == difficulty]
+        if distance:
+            df = df[df['COURS_LT_CN'] <= distance]
+        filtered_df = df[df['SIGNGU_NM'].str.contains(place)]
 
         # 필요한 컬럼만 선택
         result_df = filtered_df[['WLK_COURS_FLAG_NM', 'WLK_COURS_NM', 'COURS_DC', 'COURS_LEVEL_NM', 'COURS_LT_CN', 'COURS_TIME_CN', 'LNM_ADDR']]
