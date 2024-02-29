3b224a2e6bc87babba32e23f676e2a5bf59612d0
OHJOOWOO <juoh2011@naver.com>
2024-02-29 10:46:03 +0900

    ICT2-86식단추천완료

--- app.py
+++ app.py
@@ -11,12 +11,14 @@ from api.ChatBot import ChatBot
 # OCR서비스용
 from api.ocr import OCR
 # ServiceWorker서비스용
-from api.ServiceWorker import ServiceWorker
+#from api.ServiceWorker import ServiceWorker
 
 from api.recipe import RecipeResource
 
 from api.news import Navernews, Exercise, Nutrients
 
+#식단 추천시스템용
+from api.diet import DietRecommend
 
 app = Flask(__name__)
 CORS(app,
@@ -32,7 +34,6 @@ api = Api(app)
 @app.route('/')
 def home():
     return "Hello, ICT!"
-
 
 asgi_app = WsgiToAsgi(app)
 
@@ -50,9 +51,14 @@ api.add_resource(OCR, '/ocr')
 ServiceWorker
 Post /serviceworker
 '''
-api.add_resource(ServiceWorker, '/serviceworker')
+#api.add_resource(ServiceWorker, '/serviceworker')
 api.add_resource(ProfileResource, '/profile/img')
 api.add_resource(RecipeResource, '/recipe-info')
+'''
+RecommendAlgorithm
+Get /diet
+'''
+api.add_resource(DietRecommend,'/diet')
 
 if __name__ == '__main__':
     uvicorn.run(asgi_app, port=2222, host='0.0.0.0')
