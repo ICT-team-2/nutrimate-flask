9d764dc661a9e75b17221b7146405156e77a7838
jaehak26 <jack10829@naver.com>
2024-03-05 17:07:31 +0900

    공통으로 사용할 oracle_connection 클래스 생성

--- model/food.py
+++ model/food.py
@@ -1,10 +1,11 @@
 import cx_Oracle
 import csv
 
-# 오라클 데이터베이스 연결 설정
-dsn = cx_Oracle.makedsn('leeserver.ddns.net', '3131', service_name='xe')
-connection = cx_Oracle.connect("ICT", "ICT1234", dsn, encoding="UTF-8")
+from oracle_connection import OracleConnection
+# db -> csv로 데이터 추출하는 코드(중요x)
 
+# 오라클 데이터베이스 연결 설정
+connection = OracleConnection().connect()
 # 데이터베이스 버전 확인
 print(connection.version)
 
@@ -14,20 +15,20 @@ def insert_data_from_csv(file_path, connection):
     with open(file_path, 'r', encoding='utf-8') as file:
         reader = csv.reader(file)
         next(reader)  # 첫 번째 행(해더) 건너뛰기
-
+        
         with connection.cursor() as cursor:
             for row in reader:
                 # SQL 쿼리 준비, 테이블명과 컬럼명을 정확히 지정해야 함
                 insert_query = """
                 INSERT INTO food
-                (FOOD_ID, FOOD_NAME, FOOD_GROUP, FOOD_INTAKE, INTAKE_UNIT,
-                FOOD_CAL, FOOD_PROTEIN, FOOD_CARBO, FOOD_PROVI, FOOD_SALT, FOOD_CHOLE) 
+                (food_id, food_name, food_group, food_intake, intake_unit,
+                food_cal, food_protein, food_carbo, food_provi, food_salt, food_chole)
                 VALUES (:1, :2, :3, :4, :5, :6, :7, :8, :9, :10, :11)
                 """
-
+                
                 # 데이터 삽입
                 cursor.execute(insert_query, row)
-
+            
             # 삽입한 데이터 커밋
             connection.commit()
 
