dd21944353857964dc360ab35e2e4e6b0baa3859
jaehak26 <jack10829@naver.com>
2024-03-14 23:11:29 +0900

    https일때 당뇨/고혈압을 위한 식단 사진이 안나오는 오류 수정

--- api/health_meals.py
+++ api/health_meals.py
@@ -3,17 +3,18 @@ import pandas as pd
 import os
 import random
 
-class HealthMeal(Resource):
 
+class HealthMeal(Resource):
+    
     def diabetes_meals(self):
         # csv에서 식단 정보를 가져옴
-        df = pd.read_csv(os.path.join(os.path.dirname(__file__), './meal.csv'))
+        df = pd.read_csv(os.path.join(os.path.dirname(__file__), './meal4.csv'))
         # DataFrame을 레코드 딕셔너리로 변환
         meals_list = df.to_dict(orient='records')
         # 리스트에서 랜덤으로 3개의 식단을 선택
         random_meals = random.sample(meals_list, 3)
         return {'meals': random_meals}
-
+    
     def get(self):
         try:
             return self.diabetes_meals()
